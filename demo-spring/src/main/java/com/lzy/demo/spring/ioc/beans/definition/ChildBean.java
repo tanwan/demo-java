a720fc08e0eac757d07bb4089a97323c49f0ae86
lzy <lzytanwan@gmail.com>
2019-03-22 11:50:46 +0800

    BeanDefinition

---
+++ demo-spring/src/main/java/com/lzy/demo/spring/ioc/beans/definition/ChildBean.java
@@ -0,0 +1,16 @@
+/*
+ * Created by lzy on 2019-03-21 10:54.
+ */
+package com.lzy.demo.spring.ioc.beans.definition;
+
+import lombok.Data;
+
+/**
+ * @author lzy
+ * @version v1.0
+ */
+@Data
+public class ChildBean {
+
+    private String name;
+}
--- demo-spring/src/main/java/com/lzy/demo/spring/ioc/beans/definition/ParentBean.java
+++ demo-spring/src/main/java/com/lzy/demo/spring/ioc/beans/definition/ParentBean.java
@@ -1,16 +1,20 @@
 /*
- * Created by lzy on 2018/11/5 1:03 PM.
+ * Created by lzy on 2019-03-21 10:54.
  */
 package com.lzy.demo.spring.ioc.beans.definition;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 /**
- * 使用@Component声明的Bean
- *
  * @author lzy
  * @version v1.0
  */
+@Data
 @Component
-public class UserComponent {
+public class ParentBean {
+
+    private String name;
+
 }
+
