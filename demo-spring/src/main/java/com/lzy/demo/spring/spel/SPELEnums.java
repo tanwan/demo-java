93eeaa2834dc52c9ec9d784f9ff26c79a5664bf3
lzy <lzytanwan@gmail.com>
2019-06-03 23:37:45 +0800

    spel

---
+++ demo-spring/src/main/java/com/lzy/demo/spring/spel/SPELEnums.java
@@ -0,0 +1,21 @@
+/*
+ * Created by lzy on 2019-06-03 12:28.
+ */
+package com.lzy.demo.spring.spel;
+
+/**
+ * The enum Spel enums.
+ *
+ * @author lzy
+ * @version v1.0
+ */
+public enum SPELEnums {
+    /**
+     * Enum 1 spel enums.
+     */
+    ENUM1,
+    /**
+     * Enum 2 spel enums.
+     */
+    ENUM2;
+}
