edbda8d21f2659424e942d3bd6947a1e9b6fe9b0
lzy <lzytanwan@gmail.com>
2019-06-15 15:47:24 +0800

    spring-session

---
+++ demo-spring/src/test/java/com/lzy/demo/spring/session/SessionCookieTest.java
@@ -0,0 +1,86 @@
+/*
+ * Created by lzy on 2019-06-11 16:45.
+ */
+package com.lzy.demo.spring.session;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+/**
+ * 测试spring-session的cookie
+ *
+ * @author lzy
+ * @version v1.0
+ */
+public class SessionCookieTest {
+
+    /**
+     * 使用ServerProperties指定Session的cookie
+     */
+    @ActiveProfiles("server-properties")
+    public static class UseServerProperties extends SessionTest {
+
+        /**
+         * 测试使用ServerProperties指定Session的cookie
+         *
+         * @param mockMvc the mock mvc
+         * @throws Exception the exception
+         */
+        @Test
+        public void testUseServerProperties(@Autowired MockMvc mockMvc) throws Exception {
+            String sessionValue = "hello world";
+            // 请求创建session
+            mockMvc.perform(MockMvcRequestBuilders.get("/session/create")
+                    .param("sessionValue", sessionValue)
+            ).andExpect(result -> Assertions.assertThat(result.getResponse().getCookie("PROPERTIES_COOKIE_NAME")).isNotNull());
+        }
+    }
+
+    /**
+     * 使用java config指定Session的cookieThe type Use java config.
+     */
+    @SpringJUnitConfig(classes = UseJavaConfig.CookieConfig.class)
+    public static class UseJavaConfig extends SessionTest {
+        private final static String COOKIE_NAME = "COOKIE_NAME";
+
+        @Configuration
+        public static class CookieConfig {
+            /**
+             * 配置session的cookie
+             *
+             * @return the cookie serializer
+             */
+            @Bean
+            public CookieSerializer cookieSerializer() {
+                DefaultCookieSerializer defaultCookieSerializer = new DefaultCookieSerializer();
+                defaultCookieSerializer.setCookieName(COOKIE_NAME);
+                return defaultCookieSerializer;
+            }
+        }
+
+        /**
+         * 测试使用java config指定Session的cookie
+         *
+         * @param mockMvc the mock mvc
+         * @throws Exception the exception
+         */
+        @Test
+        public void testUseJavaConfig(@Autowired MockMvc mockMvc) throws Exception {
+            String sessionValue = "hello world";
+            // 请求创建session
+            mockMvc.perform(MockMvcRequestBuilders.get("/session/create")
+                    .param("sessionValue", sessionValue)
+            ).andExpect(result -> Assertions.assertThat(result.getResponse().getCookie(COOKIE_NAME)).isNotNull());
+        }
+    }
+}
---
+++ demo-spring/src/test/java/com/lzy/demo/spring/session/SessionTest.java
@@ -0,0 +1,110 @@
+/*
+ * Created by lzy on 2019-06-11 16:45.
+ */
+package com.lzy.demo.spring.session;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Base64;
+
+
+/**
+ * 测试spring-session
+ *
+ * @author lzy
+ * @version v1.0
+ */
+@SpringBootTest(classes = SessionTest.SessionApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = "spring.config.location=classpath:spring-session.yml")
+@AutoConfigureMockMvc
+public class SessionTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private RedisOperationsSessionRepository redisOperationsSessionRepository;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 测试session
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void testSession() throws Exception {
+        String sessionValue = "hello world";
+        // 请求创建session
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/session/create")
+                .param("sessionValue", sessionValue)
+        ).andDo(MockMvcResultHandlers.print()).andReturn();
+        // redis的session不为null
+        String cookieKey = new String(Base64.getDecoder().decode(mvcResult.getResponse().getCookie("SESSION").getValue()));
+        Assertions.assertThat(redisOperationsSessionRepository.findById(cookieKey))
+                .isNotNull();
+        // 请求获取session
+        mockMvc.perform(MockMvcRequestBuilders.get("/session/get")
+                //使用同一个cookie
+                .cookie(mvcResult.getResponse().getCookies()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sessionKey").value(sessionValue))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    /**
+     * 测试session过期
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void testSessionExpire() throws Exception {
+        String sessionValue = "hello world";
+        // 请求创建session
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/session/create")
+                .param("sessionValue", sessionValue)
+        ).andReturn();
+        // redis的session不为null
+        String cookieKey = new String(Base64.getDecoder().decode(mvcResult.getResponse().getCookie("SESSION").getValue()));
+        Assertions.assertThat(redisOperationsSessionRepository.findById(cookieKey))
+                .isNotNull();
+        // 请求获取session
+        mockMvc.perform(MockMvcRequestBuilders.get("/session/get")
+                //使用同一个cookie
+                .cookie(mvcResult.getResponse().getCookies()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sessionKey").value(sessionValue));
+
+        // 当session过期后(过期时间为3s)
+        Thread.sleep(4000);
+        // 使用RedisOperationsSessionRepository获取不到缓存
+        Assertions.assertThat(redisOperationsSessionRepository.findById(cookieKey))
+                .isNull();
+        // 但是redis的session其实还没有删除
+        // spring-session虽然默认每分钟执行一次清理redis,但是清理的只是spring:expirations:的内容
+        // 对于spring:session的内容(过期时间是session的过期时间加5分钟),只是遍历去访问redis,让redis触发清理动作,这个实现感觉spring实现得不太好
+        String redisKey = "spring:session:sessions:" + cookieKey;
+        // 这边需要的key需要使用string序列化,所以这边直接用了StringRedisTemplate
+        Assertions.assertThat(stringRedisTemplate.boundHashOps(redisKey).entries())
+                .isNotEmpty();
+    }
+
+    /**
+     * session启动类
+     */
+    @SpringBootApplication
+    @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3)
+    public static class SessionApplication {
+
+    }
+}
