b52fb7bd00658af4f1e8f0aee2ba0dae37f4e9bf
lzy <lzytanwan@gmail.com>
2019-09-21 13:46:51 +0800

    修改spring property的读取

---
+++ demo-spring/src/main/java/com/lzy/demo/spring/boot/properties/PropertySourceWithFactoryConfig.java
@@ -0,0 +1,19 @@
+/*
+ * Created by lzy on 2018/11/5 9:27 PM.
+ */
+package com.lzy.demo.spring.boot.properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+
+/**
+ * {@code @PropertySource}可以引用外部配置文件,如果引用yml,需要指定factory
+ *
+ * @author lzy
+ * @version v1.0
+ */
+@Configuration
+@PropertySource(value = "classpath:properties.yml",factory = YamlPropertyLoaderFactory.class)
+public class PropertySourceWithFactoryConfig {
+}
--- demo-spring/src/test/java/com/lzy/demo/spring/boot/properties/PropertySourceTest.java
+++ demo-spring/src/test/java/com/lzy/demo/spring/boot/properties/PropertySourceTest.java
@@ -3,29 +3,66 @@
  */
 package com.lzy.demo.spring.boot.properties;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 /**
  * {@code @PropertySource}测试
+ * 由于spring 测试并没有注册ConfigurationPropertiesBindingPostProcessor,因此需要使用@EnableConfigurationProperties
+ *
  * @author lzy
  * @version v1.0
  */
+@EnableConfigurationProperties(AtConfigurationProperties.class)
 @ExtendWith(SpringExtension.class)
-@SpringJUnitConfig(classes = PropertySourceConfig.class)
 public class PropertySourceTest {
 
     /**
-     * 测试@PropertySource
-     *
-     * @param key the key
+     * 没有使用Factory,只能导入property文件,yml文件的话,只能使用configuration.properties.key: value
      */
-    @Test
-    public void testPropertySource(@Value("${key}") String key) {
-        Assertions.assertEquals("value", key);
+    @SpringJUnitConfig(classes = {PropertySourceConfig.class})
+    public static class WithoutFactory extends PropertySourceTest {
+        /**
+         * 测试@PropertySource
+         *
+         * @param key the key
+         */
+        @Test
+        public void testPropertySource(@Autowired AtConfigurationProperties atConfigurationProperties, @Value("${key}") String key) {
+            Assertions.assertThat(key).isEqualTo("value");
+            System.out.println(atConfigurationProperties);
+            Assertions.assertThat(atConfigurationProperties)
+                    .hasFieldOrPropertyWithValue("integer", 1)
+                    .hasFieldOrPropertyWithValue("email", "99156629@qq.com")
+                    .hasFieldOrPropertyWithValue("actualValue", "expectValue");
+        }
     }
+
+    /**
+     * 使用Factory,可以导入yml文件
+     */
+    @SpringJUnitConfig(classes = {PropertySourceWithFactoryConfig.class})
+    public static class WithFactory extends PropertySourceTest {
+        /**
+         * 测试@PropertySource使用factory
+         *
+         * @param atConfigurationProperties atConfigurationProperties
+         */
+        @Test
+        public void testConfigurationPropertiesWithFactory(@Autowired AtConfigurationProperties atConfigurationProperties) {
+            System.out.println(atConfigurationProperties);
+            Assertions.assertThat(atConfigurationProperties)
+                    .hasFieldOrPropertyWithValue("integer", 1)
+                    .hasFieldOrPropertyWithValue("email", "99156629@qq.com")
+                    .hasFieldOrPropertyWithValue("actualValue", "expectValue");
+        }
+    }
+
+
 }
