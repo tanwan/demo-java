bd84a36b54713a9563e910053c40efb850b095d9
lzy <lzytanwan@gmail.com>
2019-10-12 15:45:07 +0800

    shiro

---
+++ demo-security/demo-shiro/src/main/java/com/lzy/demo/shiro/ShiroApplication.java
@@ -0,0 +1,24 @@
+/*
+ * Created by LZY on 2017/10/2 15:42.
+ */
+package com.lzy.demo.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LZY
+ * @version v1.0
+ */
+@SpringBootApplication
+public class ShiroApplication {
+
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(ShiroApplication.class, args);
+    }
+}
---
+++ demo-security/demo-shiro/src/main/java/com/lzy/demo/shiro/controller/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+/*
+ * Created by LZY on 2017/5/4 22:33.
+ */
+package com.lzy.demo.shiro.controller;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authz.AuthorizationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * 全局异常捕获
+ *
+ * @author LZY
+ * @version v1.0
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 处理认证异常
+     *
+     * @param e the e
+     * @return the response entity
+     */
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>("AuthenticationException", HttpStatus.UNAUTHORIZED);
+    }
+
+    /**
+     * 处理权限不足
+     *
+     * @param e the e
+     * @return the response entity
+     */
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<String> handleAuthorizationException(AuthorizationException e) {
+        e.printStackTrace();
+        return new ResponseEntity<>("AuthorizationException", HttpStatus.FORBIDDEN);
+    }
+}
