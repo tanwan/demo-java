74d0ff3f3f4a2b1cf4d2457f9659afe870c0ed57
lzy <lzytanwan@gmail.com>
2020-03-30 17:42:38 +0800

    spark

---
+++ demo-big-data/demo-spark/src/test/java/com/lzy/demo/spark/java/DatasetTest.java
@@ -0,0 +1,44 @@
+/*
+ * Created by lzy on 2019/11/7 8:31 AM.
+ */
+package com.lzy.demo.spark.java;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.jupiter.api.Test;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+/**
+ * The type Dataset test.
+ *
+ * @author lzy
+ * @version v1.0
+ */
+public class DatasetTest {
+    /**
+     * Test map.
+     */
+    @Test
+    public void testLocal() {
+        SparkConf conf = new SparkConf().setAppName("Test").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> input = sc.parallelize(Arrays.asList("1", "2", "3", "4"));
+        JavaRDD<Integer> inputMap = input.map(Integer::valueOf);
+        System.out.println(inputMap.count());
+    }
+
+    /**
+     * Test remote.
+     */
+    @Test
+    public void testRemote() throws UnknownHostException {
+        SparkConf conf = new SparkConf().setAppName("Test").setMaster("spark://hadoop-pseudo:7077");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> input = sc.parallelize(Arrays.asList("1", "2", "3", "4"));
+        JavaRDD<Integer> inputMap = input.map(Integer::valueOf);
+        System.out.println(inputMap.count());
+    }
+}
